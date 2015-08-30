306b4f6ec9486eb63b4d3c811719a7d2a709c005
wentan4617 <went@went-mac.local>
2015-08-30 20:52:14 +0800

    test mac

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,7 +11,7 @@ public class Main {
 	private static final Logger log = Logger.getLogger(Main.class);
 	public static void main(String[] args) {
 		//程序启动入口
-		//1启动所有服务,如果要添加配置文件的话，在ServerManager中直接读取并缓存（没做）
+		//1启动所有服务,如果要添加配置文件的话，在ServerManager中直接读取并缓存(没有实现)
 		ServerManager manager = new ServerManager();
 		try {
 			log.info("启动所有服务");
